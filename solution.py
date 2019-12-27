e3b90a3570232c0e31008a4aa8e36a3aa3e00b9d
Mikael Lind <elemel@elemel.se>
2019-12-27 08:49:50 +0100

    Spaces not tabs

--- solution.py
+++ solution.py
@@ -6,14 +6,14 @@ DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
 def print_path(grid, parents):
-	parent = parents.get(grid)
+    parent = parents.get(grid)
 
-	if parent:
-		print_path(parent, parents)
-		print()
+    if parent:
+        print_path(parent, parents)
+        print()
 
-	for row in grid:
-		print(row)
+    for row in grid:
+        print(row)
 
 
 grid = tuple(line.rstrip() for line in sys.stdin)
@@ -26,38 +26,38 @@ max_peg_count = 0
 max_grid = None
 
 while queue:
-	peg_count, grid = queue.popleft()
+    peg_count, grid = queue.popleft()
 
-	if peg_count <= max_peg_count:
-		continue
+    if peg_count <= max_peg_count:
+        continue
 
-	done = True
+    done = True
 
-	for y, row in enumerate(grid):
-		for x, cell in enumerate(row):
-			if cell == 'o':
-				for dx, dy in DIRECTIONS:
-					if (0 <= y + 2 * dy < len(grid) and
-						0 <= x + 2 * dx < len(grid[y + 2 * dy]) and
-						grid[y + dy][x + dx] == 'o' and
-						grid[y + 2 * dy][x + 2 * dx] == '.'):
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell == 'o':
+                for dx, dy in DIRECTIONS:
+                    if (0 <= y + 2 * dy < len(grid) and
+                        0 <= x + 2 * dx < len(grid[y + 2 * dy]) and
+                        grid[y + dy][x + dx] == 'o' and
+                        grid[y + 2 * dy][x + 2 * dx] == '.'):
 
-						done = False
+                        done = False
 
-						new_grid = list(list(row) for row in grid)
-						new_grid[y][x] = '.'
-						new_grid[y + dy][x + dx] = '.'
-						new_grid[y + 2 * dy][x + 2 * dx] = 'o'
-						new_grid = tuple(''.join(row) for row in new_grid)
+                        new_grid = list(list(row) for row in grid)
+                        new_grid[y][x] = '.'
+                        new_grid[y + dy][x + dx] = '.'
+                        new_grid[y + 2 * dy][x + 2 * dx] = 'o'
+                        new_grid = tuple(''.join(row) for row in new_grid)
 
-						if new_grid not in parents:
-							parents[new_grid] = grid
-							queue.append((peg_count - 1, new_grid))
+                        if new_grid not in parents:
+                            parents[new_grid] = grid
+                            queue.append((peg_count - 1, new_grid))
 
-	if done:
-		if peg_count > max_peg_count:
-			max_peg_count = peg_count
-			max_grid = grid
+    if done:
+        if peg_count > max_peg_count:
+            max_peg_count = peg_count
+            max_grid = grid
 
 print_path(max_grid, parents)
 print()
